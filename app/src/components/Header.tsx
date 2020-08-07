19a7845400537c7918f62d7bdb49e1f3e645d4e5
Soireee <soireele@gmail.com>
2020-08-07 23:35:10 +0900

    add header component

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledHeader } from './styles/StyledHeader'
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <div>Hello</div>
+      <h1>BigHello</h1>
+    </StyledHeader>
+  )
+}
---
+++ app/src/components/styles/StyledHeader.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+color: orange;
+
+h1 {
+  color: '#7c7c7c';
+}
+`
